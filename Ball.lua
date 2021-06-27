fc4ba8824dbd875ebb956162964e2653183c9179
Cristian Nita <cristiannita24@gmail.com>
2021-06-27 20:54:57 +0300

    The Ball to bounce off the Paddles and window boundaries.

--- Ball.lua
+++ Ball.lua
@@ -13,9 +13,21 @@ end
 function Ball:reset()
     self.x = VIRTUAL_WIDTH / 2
     self.y = VIRTUAL_HEIGHT / 2
-    
+
     self.dy = math.random(-50, 50)
     self.dx = math.random(2) == 1 and 100 or -100
+end
+
+function Ball:collides(paddle)
+    if self.x > paddle.x + paddle.width or paddle.x > self.x + self.width then
+        return false
+    end
+
+    if self.y > paddle.y + paddle.height or paddle.y > self.y + self.height then
+        return false
+    end
+
+    return true
 end
 
 function Ball:update(dt)
