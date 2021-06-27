bcc2605250aa7c449bfa8b859095603262c3a783
Cristian Nita <cristiannita24@gmail.com>
2021-06-27 19:43:11 +0300

    Adds interactivity to the Paddles by letting us move them up and down using the w and s keys for the left Paddle and the up and down keys for the right Paddle.

--- main.lua
+++ main.lua
@@ -7,12 +7,17 @@ WINDOW_HEIGHT = 720
 VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
 
+PADDLE_SPEED = 200
+
 -- Runs when the game first starts up, only once; used to initialize the game
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
     -- load a more 'retre-looking' font
     smallFont = love.graphics.newFont('font.ttf', 8)
+
+    -- larger font for drawing the score on the screen
+    scoreFont = love.graphics.newFont('font.ttf', 32)
 
     -- set the active font
     love.graphics.setFont(smallFont)
@@ -23,6 +28,12 @@ function love.load()
         resizable = false,
         vsync = true
     })
+
+    player1Score = 0
+    player2Score = 0
+
+    player1Y = 30
+    player2Y = VIRTUAL_HEIGHT - 50
 end
 
 -- Keyboard handling function, called by LOVE2D each frame
@@ -30,6 +41,23 @@ function love.keypressed(key)
     if key == 'escape' then
         -- terminate the application
         love.event.quit()
+    end
+end
+
+-- Runs every frame, dt is the time since the last frame
+function love.update(dt)
+    -- player 1 movement
+    if love.keyboard.isDown('w') then
+        player1Y = player1Y + -PADDLE_SPEED * dt
+    elseif love.keyboard.isDown('s') then
+        player1Y = player1Y + PADDLE_SPEED * dt
+    end
+
+    -- player 2 movement
+    if love.keyboard.isDown('up') then
+        player2Y = player2Y + -PADDLE_SPEED * dt
+    elseif love.keyboard.isDown('down') then
+        player2Y = player2Y + PADDLE_SPEED * dt
     end
 end
 
@@ -41,6 +69,7 @@ function love.draw()
     -- clear the screen with a specific color
     love.graphics.clear(40/255, 45/255, 52/255, 255/255)
 
+    love.graphics.setFont(smallFont)
     -- draw welcome text to toward the top of the screen
     love.graphics.printf(
         'Hello Pong!', -- text to render
@@ -50,11 +79,15 @@ function love.draw()
         'center' -- align mode
     );
 
+    love.graphics.setFont(scoreFont)
+    love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 3)
+    love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 50, VIRTUAL_HEIGHT / 3)
+
     -- render the first paddle
-    love.graphics.rectangle('fill', 10, 30, 5, 20)
+    love.graphics.rectangle('fill', 10, player1Y, 5, 20)
 
     -- render the second paddle
-    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 15, VIRTUAL_HEIGHT - 50, 5, 20)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 15, player2Y, 5, 20)
 
     -- end rendering at virtual resolution 
     push:finish()
