c27d41c2debffc0fd9a9dd61d0fce23d1fe5dd1c
Cristian Nita <cristiannita24@gmail.com>
2021-06-27 21:01:00 +0300

    keep track of the score

--- main.lua
+++ main.lua
@@ -86,6 +86,20 @@ function love.update(dt)
         end
     end
 
+    if ball.x < 0 then
+        servingPlayer = 1
+        player2Score = player2Score + 1
+        ball:reset()
+        gameState = 'serve'
+    end
+
+    if ball.x > VIRTUAL_WIDTH then
+        servingPlayer = 2
+        player1Score = player1Score + 1
+        ball:reset()
+        gameState = 'serve'
+    end
+
     if love.keyboard.isDown('w') then
         player1.dy = -PADDLE_SPEED
     elseif love.keyboard.isDown('s') then
