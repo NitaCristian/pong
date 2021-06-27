5897cf21853413aaac77e1ba28bcc74bdc41f83f
Cristian Nita <cristiannita24@gmail.com>
2021-06-27 19:27:26 +0300

    What our Pong program should look like. Added two paddles and a welcome message at the top of the screen.

--- main.lua
+++ main.lua
@@ -11,6 +11,12 @@ VIRTUAL_HEIGHT = 243
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
+    -- load a more 'retre-looking' font
+    smallFont = love.graphics.newFont('font.ttf', 8)
+
+    -- set the active font
+    love.graphics.setFont(smallFont)
+
     -- initialize our virtual resolution
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
@@ -32,14 +38,23 @@ function love.draw()
     -- begin rendering at virtual resolution
     push:start()
 
-    -- Print function that can align text left, right, or center on the screen.
+    -- clear the screen with a specific color
+    love.graphics.clear(40/255, 45/255, 52/255, 255/255)
+
+    -- draw welcome text to toward the top of the screen
     love.graphics.printf(
         'Hello Pong!', -- text to render
         0, -- starting X position
-        VIRTUAL_HEIGHT / 2 - 6, -- starting Y position
+        20, -- starting Y position
         VIRTUAL_WIDTH, -- number of pixels to center within
         'center' -- align mode
     );
+
+    -- render the first paddle
+    love.graphics.rectangle('fill', 10, 30, 5, 20)
+
+    -- render the second paddle
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 15, VIRTUAL_HEIGHT - 50, 5, 20)
 
     -- end rendering at virtual resolution 
     push:finish()
